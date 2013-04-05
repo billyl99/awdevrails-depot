3b6f838e5a514919093092cf0464bcb7cdf20a33
Billy Lam <blam@oversee.net>
2013-04-04 17:54:18 -0700

    p 154

--- db/migrate/20130404005148_get_line_item_price_from_cart.rb
+++ db/migrate/20130404005148_get_line_item_price_from_cart.rb
@@ -1,8 +1,8 @@
 class GetLineItemPriceFromCart < ActiveRecord::Migration
   def up
     add_column :line_items, :price, :decimal
-    LineItem.find(:all).each do |line_item|
-      line_item.update_attribute :price, line_item.product.price if line_item.product.price
+    LineItem.all.each do |line_item|
+      line_item.update_attribute :price, line_item.product.price
     end
   end
 
